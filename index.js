39ef967ca794f3206eaa4bb160a00501003ab89d
Keshawn <Keshawn9515@icloud.com>
2023-05-14 22:57:25 -0700

    used filter() method to solve error codes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+
+  function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name.toLowerCase() === str.toLowerCase());
+  }
+  
